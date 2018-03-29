586e8fbf3bf30011ebe79dd404b0b253b6770e8d
Mihai Pitu <mihai.pitu@accenture.com>
2018-03-29 17:33:29 +0300

    Finished Services Implementation - Controller & UI - TBD

---
+++ Assignments/Assignment 1/Assignment1/Assignment1/Service/Accounts/AccountService.cs
@@ -0,0 +1,19 @@
+﻿using Assignment1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Service.Accounts
+{
+    public interface IAccountService
+    {
+        bool CreateAccount(Account account);
+
+        bool TransferMoney(Account accountFrom, double amount,Account accountTo);
+
+        bool DeleteAccount(Account account);
+
+        bool PayBills(Account accountFrom,double amount);
+    }
+}
---
+++ Assignments/Assignment 1/Assignment1/Assignment1/Service/Accounts/SpendingAccountService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment1.Models;
+using Assignment1.Repository.Accounts;
+
+namespace Assignment1.Service.Accounts
+{
+    public class SpendingAccountService : IAccountService
+    {
+        private ISpendingAccountRepository spendingAccRepo;
+
+        public SpendingAccountService(ISpendingAccountRepository spendingAccRepo)
+        {
+            this.spendingAccRepo = spendingAccRepo;
+        }
+
+        public List<SpendingAccount> GetAccounts()
+        {
+            return spendingAccRepo.FindAll();
+        }
+
+        public bool CreateAccount(Account account)
+        {
+            return spendingAccRepo.Create((SpendingAccount)account);
+        }
+
+        public bool DeleteAccount(Account account)
+        {
+            return spendingAccRepo.Delete((SpendingAccount)account);
+        }
+
+        public bool PayBills(Account accountFrom, double amount)
+        {
+            SpendingAccount bill = spendingAccRepo.GetAccountById(0);
+            return TransferMoney(accountFrom, amount, bill);
+        }
+
+        public bool TransferMoney(Account accountFrom, double amount, Account accountTo)
+        {
+            double beforeMoney = accountFrom.GetAmountMoney();
+            double afterMoney = accountTo.GetAmountMoney();
+            accountFrom.SetAmountMoney(beforeMoney - amount);
+            accountFrom.SetAmountMoney(afterMoney + amount);
+            return spendingAccRepo.Update((SpendingAccount)accountFrom) && spendingAccRepo.Update((SpendingAccount)accountTo);
+        }
+    }
+}
